b89451e2eff0526ab45bd7d4bdd1d78bade8d8bf
HamenjeetKaur <HamenjeetKaur@triosstudent.com>
2024-01-17 21:20:11 -0500

    v3

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import './mystyle.css';
+
 import { useState } from 'react';
 function Square({value, onSquareClick}) {
   return <button className="square" onClick={onSquareClick}>{value}</button>;
